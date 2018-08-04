fbdcd9ca2a759c158f4efc851bc6952f08d7a7ba
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-08-04 16:53:07 +0900

    出力を日本語に変更

--- args.go
+++ args.go
@@ -8,8 +8,8 @@ import (
 func main() {
   name := os.Args[1]
   name2 := os.Args[2]
-  fmt.Println("First argument is ... " + name)
-  fmt.Println("Second argument is ... " + name2)
+  fmt.Println("最初の引数は ... " + name)
+  fmt.Println("二つ目の引数は ... " + name2)
 }
 
 /*
